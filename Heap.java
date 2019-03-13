0a195d94679854faea948d2290a1827bea465c8e
punkrock1000 <antoineracinegingras@hotmail.com>
2019-03-13 11:43:20 -0400

    Initial commit

---
+++ Heap.java
@@ -0,0 +1,113 @@
+public class Heap {
+    public static final int FRONT = 1; 
+    
+	public int[] heapList; 
+    public int size; 
+    public int maxSize; 
+
+    public Heap(int maxsize) 
+    { 
+        this.maxSize = maxsize; 
+        this.size = 0; 
+        heapList = new int[this.maxSize + 1]; 
+        heapList[0] = Integer.MIN_VALUE; 
+    } 
+
+    
+    // Get if node is a leaf
+    public boolean isLeaf(int position) 
+    { 
+        if (position >= (size / 2) && position <= size) { 
+            return true; 
+        } 
+        return false; 
+    } 
+    
+    
+    // Get position of parent node
+    public int parentNode(int position) 
+    { 
+        return position / 2; 
+    } 
+
+
+    // Get position of right child
+    public int rightBranch(int position) 
+    { 
+        return (2 * position) + 1; 
+    } 
+    
+    // Get position of left child
+    public int leftBranch(int position) 
+    { 
+        return (2 * position); 
+    } 
+  
+    //Make the heap
+    public void heap() 
+    { 
+        for (int pos = (size / 2); pos >= 1; pos--) { 
+            priorityHeap(pos); 
+        } 
+    } 
+  
+    
+    // Convert binary tree to heap
+    public void priorityHeap(int position) 
+    { 
+        if (!isLeaf(position)) { 
+            if (heapList[position] > heapList[leftBranch(position)] || heapList[position] > heapList[rightBranch(position)]) { 
+                if (heapList[leftBranch(position)] < heapList[rightBranch(position)]) { 
+                    swap(position, leftBranch(position)); 
+                    priorityHeap(leftBranch(position)); 
+                }
+                else { 
+                    swap(position, rightBranch(position)); 
+                    priorityHeap(rightBranch(position)); 
+                } 
+            } 
+        } 
+    } 
+  
+    //Insert node to heap 
+    public void insert(int element) 
+    { 
+        heapList[++size] = element; 
+        int currentNode = size; 
+        while (heapList[currentNode] < heapList[parentNode(currentNode)]) { 
+            swap(currentNode, parentNode(currentNode)); 
+            currentNode = parentNode(currentNode); 
+        } 
+    } 
+
+    //Remove function
+    public int remove() 
+    { 
+        int remove = heapList[FRONT]; 
+        heapList[FRONT] = heapList[size--]; 
+        priorityHeap(FRONT); 
+        return remove; 
+    } 
+
+    
+    // Swap nodes of the heap
+    public void swap(int position1, int position2) 
+    { 
+        int temp; 
+        temp = heapList[position1]; 
+        heapList[position1] = heapList[position2]; 
+        heapList[position2] = temp; 
+    } 
+    
+    // Print the heap 
+    public void printHeap() 
+    { 
+    	System.out.println("The Priority Binary Heap is "); 
+        for (int i = 1; i <= size / 2; i++) { 
+            System.out.println("Parent: " + heapList[i]); 
+            System.out.print("Left: " + heapList[2 * i] + "  Right: " + heapList[2 * i + 1]);
+            System.out.println("\n"); 
+        } 
+    } 
+
+}
---
+++ Main.java
@@ -0,0 +1,29 @@
+/*
+ * Tran Hai Vong Thang 40004591
+ */
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		boolean addNumber = true;
+		Heap minHeap = new Heap(15); 
+		try {
+			while(addNumber) {
+				minHeap.insert(userInput());
+			}
+		}catch(InputMismatchException error) {
+			addNumber = false;
+		}
+        minHeap.printHeap(); 
+        System.out.print("Min heap:"+minHeap.remove()+" ");
+	}
+    
+    public static int userInput() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number to add to the heap or a letter if finished adding.");
+        int key = scan.nextInt();
+        return key;
+    }
+}
